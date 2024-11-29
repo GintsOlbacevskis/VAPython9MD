dc68087a2688541c6e626de61772066cde2be87f
GintsOlbacevskis <Gints.Olbacevskis@gmail.com>
2024-11-29 22:35:54 +0200

    Added python script files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # VAPython9MD
 
 ### Traverse.py
-Recursively traverses a directory and prints its structure is a hierarchical format
+Recursively traverses a directory and prints its structure is a hierarchical format.
+Retrieve and print stats for a given file.
--- Traverse.py
+++ Traverse.py
@@ -1,6 +1,8 @@
-#import nesesery module
+#import nesesery modules
 import os
+import time
 
+#Recursively traverses a directory and prints its structure is a hierarchical format
 def traverse_dir(path, level=0):
     try:
         #List of all items in the "directory"
@@ -14,8 +16,19 @@ def traverse_dir(path, level=0):
             #If item is a "directory", traverse it recursively
             if os.path.isdir(item_path):
                 traverse_dir(item_path, level + 1)
+            elif os.path.isfile(item_path):
+                get_file_stats(item_path, level + 1)
+
     except PermissionError:
-        print("   " * level + "|-- [Nav tiesību]")
+        print("   " * level + "|-- [Nav tiesību iegūt info]")
+
+#Retrieve and print stats for a given file
+def get_file_stats(file_path, level=0):
+    try:
+        stats = os.stat(file_path)
+        print("   " * (level + 1) + f"   [Izmērs: {stats.st_size} bytes, Modificēts: {time.ctime(stats.st_mtime)}]")
+    except Exception as e:
+        print("   " * (level + 1) + f"   [Kļūda: {str(e)}]")
 
 #Main
 if __name__ == "__main__":
